517f04fbcae5bd97c40d6f75d600a19cb4683546
nikhileshdhotre <nikhileshdhotre@gmail.com>
2022-12-03 10:58:18 +0530

    demo for team

---
+++ eclipse-workspace/mavenjava/src/main/java/segroup/mavenjava/App.java
@@ -0,0 +1,13 @@
+package segroup.mavenjava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! by Nikhilesh" );
+    }
+}
